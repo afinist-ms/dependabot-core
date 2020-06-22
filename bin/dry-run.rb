6996b3ab6035858a1de5f02513f19d503f33cadb
milind009 <milind009pincha@gmail.com>
2020-06-22 15:57:19 +0530

    Update dry-run.rb

--- bin/dry-run.rb
+++ bin/dry-run.rb
@@ -355,16 +355,31 @@ def create_user_npmrc
   File.delete(npmrc_path) if File.exist?(npmrc_path)
   File.delete(yarnrc_path) if File.exist?(yarnrc_path)
 
-  
+  out_file2 = File.new(yarnrc_path, "a")
+  out_file = File.new(npmrc_path, "a")
   npmrc = $fetcher.npmrc_content.gsub("\r\n", "\n").gsub("\r", "\n").split("\n")
   registries = []
   npmrc.each do |registry| if !registry.start_with?("#") && registry.include?("registry=")
     registries.push(registry.split('=').at(1).gsub("https:", "").gsub("http:", ""))
+    registry_value = registry.split('=').at(0)
+    if !registry_value.start_with?("registry")
+      registry_value = "\"" + registry_value + "\""
+    end
+    yarnrc_content = registry_value + " " + "\"" + registry.split('=').at(1) + "\"" + "\n"
+    out_file.write(registry + "\n")
+    out_file2.write(yarnrc_content)
   end
   end
 
+  out_file2.close
   registries = registries.uniq
 
+  
+  auth = "_auth=" + Base64.encode64(":" + $options[:reg_token]).gsub("\n", "") + "\n"
+  
+  out_file.write(auth)
+  out_file.write("always-auth=true\n") 
+  out_file.close
   registries.each do |reg|
     registry_url = reg
     $options[:credentials] << {
@@ -376,16 +391,12 @@ def create_user_npmrc
     registry_password = Base64.encode64($options[:reg_token]).gsub("\n", "")
     registry_email = "xyz@abc.com"
     out_file = File.new(npmrc_path, "a")
-    out_file2 = File.new(yarnrc_path, "a")
     registry_npmrc_content = registry_url + ":username=" + registry_username + "\n"
     registry_npmrc_content += registry_url + ":_password=" + registry_password + "\n"
     registry_npmrc_content += registry_url + ":email=" + registry_email + "\n"
-    yarnrc_content = "registry " + "\"" + registry_url + "\"" + "\n"
 
     out_file.write(registry_npmrc_content)
     out_file.write(registry_npmrc_content)
-    out_file2.write(yarnrc_content)
-    out_file2.close
     out_file.close
   end
 end
