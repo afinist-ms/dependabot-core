2e2119a7d0c105cdd58853981351854e6fa71085
Giri Bhatnagar <gibhatna@microsoft.com>
2020-08-01 19:31:22 +0530

    Safety fix for rush/updater.js

--- npm_and_yarn/helpers/lib/rush/updater.js
+++ npm_and_yarn/helpers/lib/rush/updater.js
@@ -6,7 +6,18 @@ async function runRushUpdate(rootPath, shrinkwrapFilePath){
 
     return new Promise(resolve => {
     
-        exec('node common/scripts/install-run-rush.js update --no-link --bypass-policy', { maxBuffer: 1024 * 1024 * 50 }, function(a,b,c) {
+        exec('node common/scripts/install-run-rush.js update --no-link --bypass-policy', { maxBuffer: 1024 * 1024 * 50 }, function(error, stdout, stderr) {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                console.log(`stderr: ${stderr}`);
+                console.log(`stdout: ${stdout}`);
+
+
+                // throw error;
+                // throw error;
+                return resolve(null);
+            }
+
             const updateFileContent = fs.readFileSync(path.join(rootPath, shrinkwrapFilePath)).toString()
             // return { shrinkwrapFilePath: updateFileContent };
             return resolve(updateFileContent);
